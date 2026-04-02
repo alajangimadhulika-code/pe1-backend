52711130ba9cde0946f18d4e18297a957c36da1a
alajangimadhulika-code <alajangimadhulika@gmail.com>
2026-04-02 15:50:57 +0530

    Fix CORS configuration to allow all headers

--- src/main/java/com/example/placementeligibilty/confing/CorsConfig.java
+++ src/main/java/com/example/placementeligibilty/confing/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("https://p1-frontend-anu9.onrender.com")
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };

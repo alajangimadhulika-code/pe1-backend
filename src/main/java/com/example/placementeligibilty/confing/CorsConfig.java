09b021430998089cddf2e9039cdb280bd1ed06dd
alajangimadhulika-code <alajangimadhulika@gmail.com>
2026-04-02 14:58:38 +0530

    Update CORS configuration to allow new origin

--- src/main/java/com/example/placementeligibilty/confing/CorsConfig.java
+++ src/main/java/com/example/placementeligibilty/confing/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins("https://p1-frontend-anu9.onrender.com")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
